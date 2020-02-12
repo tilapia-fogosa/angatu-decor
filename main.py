471644b9126b53cce9c5035ec21f678e0bd4e171
tilapia-fogosa <60949448+tilapia-fogosa@users.noreply.github.com>
2020-02-11 21:43:31 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,84 @@
+
+
+
+import smtplib
+from email.mime.text import MIMEText
+import mimetypes
+import os
+
+
+from email import encoders
+from email.mime.audio import MIMEAudio
+from email.mime.base import MIMEBase
+from email.mime.image import MIMEImage
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+
+
+def adiciona_anexo(msg, filename):
+    if not os.path.isfile(filename):
+        return
+
+    ctype, encoding = mimetypes.guess_type(filename)
+
+    if ctype is None or encoding is not None:
+        ctype = 'application/octet-stream'
+
+    maintype, subtype = ctype.split('/', 1)
+
+    if maintype == 'text':
+        with open(filename) as f:
+            mime = MIMEText(f.read(), _subtype=subtype)
+    elif maintype == 'image':
+        with open(filename, 'rb') as f:
+            mime = MIMEImage(f.read(), _subtype=subtype)
+    elif maintype == 'audio':
+        with open(filename, 'rb') as f:
+            mime = MIMEAudio(f.read(), _subtype=subtype)
+    else:
+        with open(filename, 'rb') as f:
+            mime = MIMEBase(maintype, subtype)
+            mime.set_payload(f.read())
+
+        encoders.encode_base64(mime)
+
+    mime.add_header('Content-Disposition', 'attachment', filename=filename)
+    msg.attach(mime)
+
+def mandar_email(assunto,destinatario,texto,anexo):
+   
+    
+    # conexão com os servidores do google
+    smtp_ssl_host = 'smtp.gmail.com'
+    smtp_ssl_port = 465
+    # username ou email para logar no servidor
+    username = 'angatudecor'
+    password = 'baal00die'
+    
+    from_addr = 'angatudecor@gmail.com'
+    to_addrs = [destinatario]
+    
+    # a biblioteca email possuí vários templates
+    # para diferentes formatos de mensagem
+    # neste caso usaremos MIMEText para enviar
+    # somente texto
+    message = MIMEMultipart()
+    message['subject'] = assunto
+    message['from'] = from_addr
+    message['to'] = ', '.join(to_addrs)
+    
+    
+    message.attach(MIMEText(str(texto)))
+    
+    adiciona_anexo(message, anexo)
+    # conectaremos de forma segura usando SSL
+    server = smtplib.SMTP_SSL(smtp_ssl_host, smtp_ssl_port)
+    # para interagir com um servidor externo precisaremos
+    # fazer login nele
+    server.login(username, password)
+    server.sendmail(from_addr, to_addrs, message.as_string())
+    server.quit()
+
+if __name__ == "__main__":
+    mandar_email('teste','igorcpereira@gmail.com','testando', 'teste.txt')
